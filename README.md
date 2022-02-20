441ac9ca4015cc5774b25218a430945c99c5ea62
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2022-02-20 01:11:32 +0100

    [INIT] first step to greatness

---
+++ README.md
@@ -0,0 +1,5 @@
+# Docta Consultancy Landing Page
+
+This is a design of a shot gotten from Dribble. [This is the link to the design](https://dribbble.com/shots/15908782-Medical-Website-Landing-Page-Design-Docta-Consultation).
+
+The live demo can be found [here](https://detachedsoul.github.io/docta)
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "brand-purple": "#2c2235",
+        "brand-pink": "#d3bebd",
+        "light-pink": "#f8f3f0",
+        "brand-indigo": "#5a6072"
+      },
+      fontFamily: {
+				"pacifico": "Pacifico, cursive",
+				"open-sans": "'Open Sans', sans-serif",
+				"poppins": "'Poppins', sans-serif",
+				"luckiest-guy": "'Luckiest Guy', cursive",
+				"mulish": "'Mulish', sans-serif",
+			}
+    },
+  },
+  plugins: [],
+}
