e072d8fe7c901a921175dd70ec463bf30fb4a220
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2022-02-20 03:08:15 +0100

    fix: made color scheme match original design closer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
 module.exports = {
-  content: [
-    "./*.html"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "brand-purple": "#2c2235",
-        "brand-pink": "#d3bebd",
-        "light-pink": "#f8f3f0",
-        "brand-indigo": "#5a6072"
-      },
-      fontFamily: {
-				"pacifico": "Pacifico, cursive",
+	content: ["./*.html"],
+	theme: {
+		extend: {
+			colors: {
+				"brand-purple": "#2c2235",
+				"brand-pink": "#d3bebd",
+				"light-pink": "#f8f3f0",
+				"brand-indigo": "#72569d",
+			},
+			fontFamily: {
+				pacifico: "Pacifico, cursive",
 				"open-sans": "'Open Sans', sans-serif",
-				"poppins": "'Poppins', sans-serif",
+				poppins: "'Poppins', sans-serif",
 				"luckiest-guy": "'Luckiest Guy', cursive",
-				"mulish": "'Mulish', sans-serif",
-			}
-    },
-  },
-  plugins: [],
-}
+				mulish: "'Mulish', sans-serif",
+			},
+		},
+	},
+	plugins: [
+		require("@tailwindcss/forms")({
+			strategy: "class",
+		}),
+	],
+};
